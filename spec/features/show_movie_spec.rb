d3506c4be96c7ca547cb73301328d7b08b30034a
Michael Wiss <michael.wiss@gmail.com>
2015-05-17 17:19:13 -0500

    added flop? method

--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -22,6 +22,12 @@ describe "Viewing an individual movie" do
      expect(page).to have_text("$60,000,000.00")
   end
 
-  it "shows 'Flop!' if the total gross is less than $50M"
+  it "shows 'Flop!' if the total gross is less than $50M" do
+  	 movie = Movie.create(movie_attributes(total_gross: 40000000.00))
+
+     visit movie_url(movie)
+
+     expect(page).to have_text("Flop!")
+  end
 
 end

f71ea54143e20be769cfc08c4c782dbfe5f83d44
Michael Wiss <michael.wiss@gmail.com>
2015-05-18 19:06:47 -0500

    added route and link_to to show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get "movies" => "movies#index"
   get "movies/:id" => "movies#show", as: "movie"
+  get "movies/:id/edit" => "movies#edit", as: "edit_movie"
   root "movies#index"
+
 end
 
